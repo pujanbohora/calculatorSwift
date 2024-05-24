231953377ac63f04341e755eca618d028f8eb0fa
Pujan Bohora <bohorapawan@gmail.com>
2024-05-24 20:57:02 +0545

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Pawan Bohora on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
